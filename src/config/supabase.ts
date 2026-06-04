8db794758cebf245b0ff2d4db00bf9ccf126aa66
marui1920 <63126406+marui1920@users.noreply.github.com>
2026-06-04 15:57:40 +0800

    update supabase config

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -1,4 +1,4 @@
 export const supabaseConfig = {
   url: '',
-  anonKey: '',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVqd2djZ211a3Z3cHJmcnB5ZWR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA1NTE5ODgsImV4cCI6MjA5NjEyNzk4OH0.5WXoR5c_fbxyIIIYxboJ9RwZqObND54aPk925sK_Xmo',
 };
